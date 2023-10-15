96f9bc6ed88521c17c2f7508ad9eb0e24ea7896f
kerrrbo <147833780+kerrrbo@users.noreply.github.com>
2023-10-15 23:04:20 +0900

    Add files via upload

---
+++ string.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+    <h1> String & String Operator</h1>
+<?php
+echo "Hello \"w\"orld";
+?>
+    <h2> concatenation operator</h2>
+    <?php
+    echo "hello "."world";
+    ?>
+    <h2> string length function</h2>
+    <?php
+    echo strlen("Hello world");
+    ?>
+</html>
+</body>

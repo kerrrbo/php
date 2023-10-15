f736763aeabe8cce23eabd62391962908dd7820d
kerrrbo <147833780+kerrrbo@users.noreply.github.com>
2023-10-15 23:20:32 +0900

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,11 @@
+<h1>variable</h1>
+      <?php
+        $name = " kerrbo ";
+        echo "Lorem ipsum dolor sit amet c
+        onsectetur".$name."adipisicing elit.
+        Nam itaque aliquam laudantium 
+        cupiditate".$name."perspiciatis,
+        maxime odio aspernatur".$name."repudiandae
+        kerrbo, nulla nemo inventore velit laborum. Aliquam ea a, voluptates 
+        kerrbo molestias".$name."blanditiis reprehenderit!";
+   ?>

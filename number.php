d9bfffc38ab1d0d83afb6fbf406acbcfb0ce12f8
kerrrbo <147833780+kerrrbo@users.noreply.github.com>
2023-10-15 22:50:58 +0900

    php 강의 시작!

---
+++ number.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+    <h1>1+1</h1>
+<?php
+echo 1+1; // 표현식
+?>
+    <h1>2-1</h1>
+<?php
+echo 2-1; // + - * / : 연산자
+?>
+    <h1>4/2</h1>
+<?php
+echo 4/2;
+?>
+</body>
+</html>
